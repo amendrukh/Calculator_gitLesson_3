11a6f296a03099c9c767e87c9f7b46ed69305206
Anna Mendrukh <amendrukh@gmail.com>
2023-05-09 18:39:15 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,20 @@
+let firstNumber = +prompt("Write first number", "0");
+let secondNumber = +prompt("Write second number", "0");
+let sign = prompt("Write a sign +, -, /, *", "+");
+let result;
+if (sign === "+") {
+    result = firstNumber + secondNumber;
+    alert(`Результат ${result}`);
+} else if (sign === "-") {
+    result = firstNumber - secondNumber;
+    alert(`Результат ${result}`);
+} else if (sign === "/") {
+    result = firstNumber / secondNumber;
+    if (secondNumber === 0) {
+        result = "Операція не можлива!На нуль ділити не можна."
+    }
+    alert(`Результат ${result}`);
+} else if (sign === "*") {
+    result = firstNumber * secondNumber;
+    alert(`Результат ${result}`);
+}
